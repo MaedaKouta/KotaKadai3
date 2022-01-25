b96977dc269d7bfc04f69f6b1393cd6b760aa980
MaedaKouta <maedakoutaa@gmail.com>
2022-01-25 14:18:14 +0900

    提出用のコミット

--- KotaKadai3/ViewController.swift
+++ KotaKadai3/ViewController.swift
@@ -9,11 +9,33 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    @IBOutlet private weak var leftTextField: UITextField!
+    @IBOutlet private weak var rightTextField: UITextField!
+    @IBOutlet private weak var leftSwitch: UISwitch!
+    @IBOutlet private weak var rightSwitch: UISwitch!
+    @IBOutlet private weak var leftTextLabel: UILabel!
+    @IBOutlet private weak var rightTextLabel: UILabel!
+    @IBOutlet private weak var resultTextLabel: UILabel!
+
+    @IBAction private func didTapButton(_ sender: Any) {
+        let leftBeforeNum = Int(leftTextField.text ?? "") ?? 0
+        let rightBeforeNum = Int(rightTextField.text ?? "") ?? 0
+        let leftAfterNum = valueConversion(num1: leftBeforeNum, simbolJudge: leftSwitch.isOn)
+        let rightAfterNum = valueConversion(num1: rightBeforeNum, simbolJudge: rightSwitch.isOn)
+
+        leftTextLabel.text = String(leftAfterNum)
+        rightTextLabel.text = String(rightAfterNum)
+        resultTextLabel.text = String(leftAfterNum + rightAfterNum)
     }
 
+    // UISwitchにあわせて符号を変換する
+    func valueConversion(num1: Int, simbolJudge: Bool) -> Int {
+        if simbolJudge == true {
+            return -1*num1
+        }else{
+            return num1
+        }
+    }
 
 }
 
